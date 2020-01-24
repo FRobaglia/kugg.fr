f1939cede38836b5aaa2f04a78acdf8d275110ea
frobaglia <felix.robaglia@gmail.com>
2020-01-24 17:00:40 +0100

    add tags start

---
+++ src/components/Tags.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Tags extends React.Component {
+  render() {
+    return (
+      <div className="tags">
+        <div class="tag">
+          Helloi
+        </div> 
+      </div>
+    )
+  }
+}
+
+export default Tags
